252e05fcc19e7f2fcc17abfdb1a2592f59b3a725
Nikolai <e12318680@student.tuwien.ac.at>
2026-06-01 11:17:11 +0200

    finishing ui and layout of shiny app without added functionality

--- assignment4/assignment4_group67_shinywebapp/app.R
+++ assignment4/assignment4_group67_shinywebapp/app.R
@@ -8,29 +8,113 @@
 #
 
 library(shiny)
+library(ggplot2)
+library(plotly)
+library(DT)
 
-# Define UI for application that draws a histogram
+data_cia <- jsonlite::fromJSON("data_cia2.json")
+print(str(data_cia))
+
+data_cia_clean <- data.frame(
+  "Country" = data_cia$country,
+  "Continent" = data_cia$continent,
+  "Subcontinent" = data_cia$subcontinent,
+  "Income status" = data_cia$status,
+  "Expenditure on education" = data_cia$expenditure,
+  "Youth unemployment rate" = data_cia$youth_unempl_rate,
+  "Net migration rate" = data_cia$net_migr_rate,
+  "Electricity from fossil fuels" = data_cia$electricity_fossil_fuel,
+  "Area" = data_cia$area,
+  "Population growth rate" = data_cia$pop_growth_rate,
+  "Life expectancy" = data_cia$life_expectancy,
+  "Population" = data_cia$population,
+  "ISO3" = data_cia$ISO3,
+  check.names = FALSE
+)
+selection_variables <- c(
+  "Expenditure on education",
+  "Youth unemployment rate",
+  "Net migration rate",
+  "Population growth rate",
+  "Electricity from fossil fuels",
+  "Life expectancy"
+)
+
+print(str(data_cia_clean))
+
+
+# Define UI for application of assignment 4
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("CIA World Factbook 2020"),
+    p("Welcome to my shiny app, which allows you to visualize variables 
+      from the CIA 2020 data set using univariate, 
+      multivariate and geographical graphics."),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    tabsetPanel(
+      # Univariate tab
+      tabPanel(
+        "Univariate analysis",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              inputId = "select_univar_variable",
+              label = "Select a variable:",
+              choices = selection_variables
+              ),
+            actionButton("btnRawData", "View raw data"),
+            DTOutput("raw_dataTable")
+          ),
+          mainPanel(
+            tabsetPanel(
+              tabPanel("Map", plotlyOutput("map_plot")),
+              tabPanel(
+                "Global analysis",
+                fluidRow(
+                  column(6, plotlyOutput("hist_density_plot")),
+                  column(6, plotlyOutput("boxplot_global"))
+                )
+              ),
+              tabPanel(
+                "Analysis per continent",
+                fluidRow(
+                  column(6, plotlyOutput("density_continent_plot")),
+                  column(6, plotlyOutput("boxplot_continent"))
+                )
+              )
+            )
+          )
         )
+      ),
+      # Multivariate tab
+      tabPanel(
+        "Multivariate analysis",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              inputId = "select_multivar_xvar",
+              label = "Select variable 1:",
+              choices = selection_variables
+              ),
+            selectInput(
+              inputId = "select_multivar_yvar",
+              label = "Select variable 2:",
+              choices = selection_variables
+              ),
+            selectInput(
+              inputId = "select_multivar_scale",
+              label = "Scale points by:",
+              choices = c("Area" = "area", "Population" = "population")
+              )
+            ),
+            mainPanel(
+              plotlyOutput("scatter_plot")
+            )
+          )
+        )
+      )
     )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
